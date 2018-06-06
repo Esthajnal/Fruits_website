823df9c134dbee66e380ccb1df6c08bd8b2f5f22
Gyarmathy Eszter <tucs2010@gmail.com>
2018-06-06 22:16:09 +0200

    js buttonmake first level

---
+++ fruitsjs/fruitscript.js
@@ -0,0 +1,15 @@
+//gombkészítés
+var objplaceFruitsButton = document.querySelector('#placeFruitsButton');
+
+function makeFruitsButton() {
+  var objfruitsbutton = document.createElement('BUTTON');
+  var objButtonImg = document.createElement('IMG');
+
+  objplaceFruitsButton.appendChild(objfruitsbutton);
+  objfruitsbutton.classList.add('fruitsbutton');
+  objfruitsbutton.appendChild(objButtonImg);
+  objButtonImg.setAttribute('src', buttonImagesPlace[i];
+
+    alert('eddig ügyes')
+
+  }
--- fruitsjs/fruitstype.js
+++ fruitsjs/fruitstype.js
@@ -1,52 +1,66 @@
-var apples = [{
+var fruits = [{
+    fruittype: 'apple',
     picture: "../Imagines/alma_florina.jpg",
     typename: 'Florina',
     ripening_time: 'september-oktober',
     average_weight: 135,
   },
   {
+    fruittype: 'apple',
     picture: '../Imagines/granny_smith.jpg',
     typname: 'Granny Smith',
     ripenin_time: 'october-november',
     average_weight: 155,
   },
   {
+    fruittype: 'apple',
     picture: '../Imagines/idared.jpg',
     typname: 'Idared',
     ripenin_time: 'september-october',
     average_weight: 185,
   },
   {
+    fruittype: 'apple',
     picture: '../Imagines/jonagold.jpg',
     typname: 'Jonagold',
     ripenin_time: 'september-october',
     average_weight: 235,
-  }
-]
-var watermelon = [{
+  },
+  {
+    fruittype: 'watermelon',
     picture: '../Imagines/dinnye_crimstar.jpg',
     typename: 'Crimstar',
     ripenin_time: 'june',
     average_weight: 7,
   },
   {
-    picture: '../Imagines/dinnye_fire_ball.jpg,
+    fruittype: 'watermelon',
+    picture: '../Imagines/dinnye_fire_ball.jpg',
     typename: 'Fire Ball F1',
     ripenin_time: 'july',
     average_weight: 7.5,
   },
   {
-    picture: '../Imagines/dinnye_orange_king_f1.jpg,
+    fruittype: 'watermelon',
+    picture: '../Imagines/dinnye_orange_king_f1.jpg',
     typename: 'Orange King F1',
     ripenin_time: 'end of july',
     average_weight: 9,
   },
   {
+    fruittype: 'watermelon',
     picture: '../Imagines/dinnye_style_f1.jpg',
     typename: 'Style F1',
     ripenin_time: 'july',
     average_weight: 6,
   }
+]
+var buttonImagesPlace = [
+  apple: '../Imagines/Button_backgrounds/apple_button.jpg',
+  watermelon: '../Imagines/Button_backgrounds/dinnye_button.jpg',
+  raspberry: '../Imagines/Button_backgrounds/raspberry_button.jpg',
+  strawberry: '../Imagines/Button_backgrounds/strawberry_button.jpg',
+
 ]
 /*var raspberry = [{
   picture: ,
