3d13de8c3e61785afc6c5a602a503c04efb65b2a
Gyarmathy Eszter <tucs2010@gmail.com>
2018-06-06 19:31:02 +0200

    apple and wtermelon-types

---
+++ fruitsjs/fruitstype.js
@@ -0,0 +1,71 @@
+var apples = [{
+    picture: "../Imagines/alma_florina.jpg",
+    typename: 'Florina',
+    ripening_time: 'september-oktober',
+    average_weight: 135,
+  },
+  {
+    picture: '../Imagines/granny_smith.jpg',
+    typname: 'Granny Smith',
+    ripenin_time: 'october-november',
+    average_weight: 155,
+  },
+  {
+    picture: '../Imagines/idared.jpg',
+    typname: 'Idared',
+    ripenin_time: 'september-october',
+    average_weight: 185,
+  },
+  {
+    picture: '../Imagines/jonagold.jpg',
+    typname: 'Jonagold',
+    ripenin_time: 'september-october',
+    average_weight: 235,
+  }
+]
+var watermelon = [{
+    picture: '../Imagines/dinnye_crimstar.jpg',
+    typename: 'Crimstar',
+    ripenin_time: 'june',
+    average_weight: 7,
+  },
+  {
+    picture: '../Imagines/dinnye_fire_ball.jpg,
+    typename: 'Fire Ball F1',
+    ripenin_time: 'july',
+    average_weight: 7.5,
+  },
+  {
+    picture: '../Imagines/dinnye_orange_king_f1.jpg,
+    typename: 'Orange King F1',
+    ripenin_time: 'end of july',
+    average_weight: 9,
+  },
+  {
+    picture: '../Imagines/dinnye_style_f1.jpg',
+    typename: 'Style F1',
+    ripenin_time: 'july',
+    average_weight: 6,
+  }
+]
+/*var raspberry = [{
+  picture: ,
+  typename: ,
+  ripenin_time: ,
+  average_weight: ,
+} {
+  picture: ,
+  typename: ,
+  ripenin_time: ,
+  average_weight: ,
+} {
+  picture: ,
+  typename: ,
+  ripenin_time: ,
+  average_weight: ,
+} {
+  picture: ,
+  typename: ,
+  ripenin_time: ,
+  average_weight: ,
+}]*/
